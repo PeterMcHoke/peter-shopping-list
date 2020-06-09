40d7fc8175f70d26277fe942d4dfbeeee76c92cf
PeterMcHoke <petermchoke@gmail.com>
2020-06-09 12:35:50 -0500

    Implementing Version 2.0 of the shopping list app

--- script.js
+++ script.js
@@ -1,32 +1,98 @@
-$(() => {
-  $('#js-shopping-list-form').on('click','button', e => {
-      e.preventDefault();
-      const userInput = $('#shopping-list-entry').val();
-      $('.shopping-list').append(`<li class="item-container">
-        <span class="shopping-item">${userInput}</span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
+'use strict';
+
+const STORE = [
+  {id: cuid(), name: "apples", checked: false},
+  {id: cuid(), name: "oranges", checked: false},
+  {id: cuid(), name: "milk", checked: true},
+  {id: cuid(), name: "bread", checked: false}
+];
+
+
+function generateItemElement(item) {
+  return `
+    <li data-item-id="${item.id}">
+      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
             <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
+        </button>
+        <button class="shopping-item-delete js-item-delete">
             <span class="button-label">delete</span>
-          </button>
-        </div>
-      </li>`);
-      $('#shopping-list-entry').val("");
-    });
-    //
-    $('.shopping-list').on('click', 'li > .shopping-item-controls > button.shopping-item-toggle', function(e) {
-      $(this).closest('li').children('span.shopping-item').toggleClass('shopping-item__checked');
-    });
+        </button>
+      </div>
+    </li>`;
+}
 
-    $('.shopping-list').on('click', 'li > .shopping-item-controls > button.shopping-item-delete', function(e) {
-      $(this).closest('li').remove();
-    });
 
-});
+function generateShoppingItemsString(shoppingList) {
 
-//When a user types something in to the form, and hits submit
-//Create an li with a shopping-item inside of it
-//When a user hits check, toggle the class .shopping-item__checked
-//When a user hits delete .remove() $(this)
+  const items = shoppingList.map((item) => generateItemElement(item));
+
+  return items.join("");
+}
+
+
+function renderShoppingList() {
+  // render the shopping list in the DOM
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
+
+  // insert that HTML into the DOM
+  $('.js-shopping-list').html(shoppingListItemsString);
+}
+
+function addItemToShoppingList(itemName) {
+  STORE.push({id: cuid(), name: itemName, checked: false});
+}
+
+function handleNewItemSubmit() {
+  $('#js-shopping-list-form').on('submit', e => {
+    e.preventDefault();
+    const newItemName = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val("");
+    addItemToShoppingList(newItemName);
+    renderShoppingList();
+  });
+}
+
+function toggleCheckedForListItem(itemId) {
+  const item = STORE.find(item => item.id === itemId);
+  item.checked = !item.checked;
+}
+
+function getItemIdFromElement(item){
+  const id = $(item).closest('li').data('item-id');
+  return id;
+};
+
+function handleItemCheckClicked() {
+  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+    const id = getItemIdFromElement(event.currentTarget);
+    toggleCheckedForListItem(id);
+    renderShoppingList();
+  });
+}
+
+function toggleDeleteListItem(itemId) {
+  const itemIndex = STORE.findIndex(item => item.id === itemId);
+  STORE.splice(itemIndex,1);
+};
+
+function handleDeleteItemClicked() {
+  $('.js-shopping-list').on('click', `.js-item-delete`, event => {
+    const id = getItemIdFromElement(event.currentTarget);
+    toggleDeleteListItem(id);
+    renderShoppingList();
+  })
+};
+
+
+function handleShoppingList() {
+  renderShoppingList();
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
+
+};
+
+// when the page loads, call `handleShoppingList`
+$(handleShoppingList);
